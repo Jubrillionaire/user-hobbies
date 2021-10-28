fdd99ebc538ea21b9121b22a0807868da815d8d9
jubrillionaire <54190346+Jubrillionaire@users.noreply.github.com>
2021-10-28 08:42:57 +0100

    Update Hobbies.tsx

--- frontend/src/component/Hobbies.tsx
+++ frontend/src/component/Hobbies.tsx
@@ -10,45 +10,45 @@ interface Iprops {
 }
 
 
-const Hobbies :  FC <Iprops>  = ({selectedUser}) => {
-  
+const Hobbies: FC<Iprops> = ({ selectedUser }) => {
 
-    interface Istate {
-        user: string
-       name: string,
-       year: number | null,
-       passionLevel: string
+
+  interface Istate {
+    user: string
+    name: string,
+    year: number,
+    passionLevel: string
   }
 
 
   const dispatch = useDispatch();
-  
 
-  const {hobbies} = useSelector((state: RootState) => state.hobbies)
+
+  const { hobbies } = useSelector((state: RootState) => state.hobbies)
 
   const [formData, setFormData] = useState<Istate>({
-       user: "",
-       name: "",
-       year: null,
-       passionLevel: ""
+    user: "",
+    name: "",
+    year: 0,
+    passionLevel: ""
   })
 
   const clearForm = () => {
-    setFormData({...formData,
+    setFormData({
       user: "",
       name: "",
-      year: null,
+      year: 0,
       passionLevel: ""
     })
   }
 
-  
 
-  
+
+
 
   const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const {name, year, passionLevel} = formData
+    const { name, year, passionLevel } = formData
 
     const apiData = {
       user: selectedUser,
@@ -57,68 +57,67 @@ const Hobbies :  FC <Iprops>  = ({selectedUser}) => {
       passionLevel
     }
 
-    if(formData){
-        dispatch(addHobby(apiData));
-    }else{
-      
-        alert("please enter a value")      
-        }
+    if (selectedUser) {
+      dispatch(addHobby(apiData));
+    } else {
 
-        clearForm()
-  
-};
+      alert("please select a user")
+    }
+clearForm()
+
+  };
 
 
-    
+
 
 
 
   const handleChange = (event: FormEvent<HTMLInputElement | HTMLSelectElement>) => {
-           setFormData({ ...formData, [event.currentTarget.name]: event.currentTarget.value })
-}
+    setFormData({ ...formData, [event.currentTarget.name]: event.currentTarget.value })
+  }
 
-const handleDelete = (id: string, user_id:string) => {
-  if(window.confirm("are you sure you want to delete?")){
-    dispatch(deleteHobby(id, user_id))
-  };
+  const handleDelete = (id: string, user_id: string) => {
+    if (window.confirm("are you sure you want to delete?")) {
+      dispatch(deleteHobby(id, user_id))
+    };
 
-}
+  }
 
 
   return (
     <>
       <div className="table__column-hobbies">
         <form onSubmit={handleSubmit}>
-          <select name='passionLevel' onChange={handleChange} value={formData.passionLevel}>
-          <option>Passion Level</option>
+          <select required name='passionLevel' onChange={handleChange} value={formData.passionLevel}>
+            <option value="">Passion Level</option>
             <option value="low">Low</option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
             <option value="very-high">Very High</option>
           </select>
-         <input required onChange={handleChange} name="name" placeholder='enter name' type="text" />
-          <input required onChange={handleChange} name="year" placeholder='enter year' type="number" />
+          <input value={formData.name} required onChange={handleChange} name="name" placeholder='enter name' type="text" />
+          <input value={formData.year} required onChange={handleChange} name="year" placeholder='enter year' type="number" />
           <button>Add</button>
         </form>
         {hobbies.length > 0 ? (
-        <div className="table__column-hobbies-container">
-          <ul>
-          {hobbies.map((hobby: hobbyInfo) => (
-             <li className="table__column-hobbies-group">
-             <div>Passion: {hobby.passionLevel}</div> 
-            {hobby.name}
-             <div>       Since {hobby.year} <span onClick={() => handleDelete(hobby._id, hobby.user)} ><i className="fas fas-trash"></i>delete</span>
-             </div>
-           </li>
-          ))}
-           
-          </ul>
-        </div>
+          <div className="table__column-hobbies-container">
+            <ul>
+              {hobbies && hobbies.map((hobby: hobbyInfo) => (
+                <li className="table__column-hobbies-group">
+                  <div>Passion: {hobby.passionLevel}</div>
+                  {hobby.name}
+                  <div>       Since {hobby.year} <span onClick={() => handleDelete(hobby._id, hobby.user)} ><i className="fas fas-trash"></i>delete</span>
+                  </div>
+                </li>
+              ))}
+
+            </ul>
+          </div>
         )
-        : (
-          <div className="blank"></div>
-        )
-}
+          : (
+            <div className="blank"></div>
+          )
+        }
       </div>
     </>
   );
