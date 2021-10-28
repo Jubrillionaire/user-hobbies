4a9da3651163746db6bd521b3bf50471b472fb03
jubrillionaire <54190346+Jubrillionaire@users.noreply.github.com>
2021-10-28 13:02:21 +0100

    Update hobbies-controller.ts

--- backend/controllers/hobbies-controller.ts
+++ backend/controllers/hobbies-controller.ts
@@ -16,17 +16,17 @@ const deleteHobby = async (req: Request, res: Response) => {
     try {
         const id: string = req.params.id;
         const user_id: string = req.params.user_id
-        const user = await User.findOne({_id: user_id})
+        const user = await User.findOne({ _id: user_id })
         user.hobbies.filter(i => i._id == user_id)
-
         await user.save()
 
         const deleted = await Hobby.findByIdAndDelete(id);
-            return res.status(204).send("Hobby deleted");
+        return res.status(204).send({msg: "Hobby deleted", deleted});
     } catch (error) {
         return res.send(error)
     }
 };
+
 
 
 const createHobby = async (req: Request, res: Response) => {
